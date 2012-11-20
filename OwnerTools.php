27f51a41ec0686af5dc6f87c9584b0a62decf5b4
enehring <enehring1@verizon.net>
2012-11-20 12:01:35 -0500

    Added skeleton of OwnerTools.php

---
+++ OwnerTools.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+// Get dbConn functions
+
+class OwnerTools
+{
+    function addItem()
+    {
+
+    }
+
+    function removeItem()
+    {
+
+    }
+
+    function editItem()
+    {
+
+    }
+
+    function listOrders()
+    {
+
+    }
+}
+?>
