c7f4a3953b335cdca6284a824359aa2c28751460
James Miller <jmiller8@ite240pc23.cs.umbc.edu>
2012-11-20 13:01:26 -0500

    Added html stuff

--- OwnerTools.php
+++ OwnerTools.php
@@ -1,6 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Wiki</title>
+  </head>
+  <img src="logo.png" alt="ShopMaster">
+    <div id="sidebar">
+      <h2>Navigation</h2>
+      <div class="section">
+        <ul>
+          <li><a href="./OwnerTools.php?page=test">This is a php test</a></li>
+        </ul>
+      </div>
+    </div>
+
+  </body>
+</html>
+
 <?php
-session_start();
+#session_start();
 // Get dbConn functions
+
+print "<p>HELLO</p>";
+
+/*
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+   if ($_GET['page'])
+   {
+      if ($_GET['page'] == 'test')
+      {
+         print "<p>You clicked me</p>";
+      }
+   }
+}
+*/
 
 class OwnerTools
 {
