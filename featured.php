4fb759a126cc623a0dd6fb009f27f094d3275084
Akiyukio <ply1@umbc.edu>
2012-11-27 00:56:15 -0500

    Ideas added
    Going to zzz.

--- featured.php
+++ featured.php
@@ -45,7 +45,27 @@
     	echo $row['UnitPrice'];
     	/*
     	 * This is where I am going to need to use some HTML to format things.
-    	 * */
+    	 * I want to have:
+    	 * - a layout of six items in the center
+    	 * - cannot all be the same,
+    	 * - cannot be random items
+    	 * 	- can be random featured items if there are more than 6
+    	 * - can be in random order hence the above statement
+    	 * 
+    	 * That said, here is some idea code:
+<!-- Here is the SALES section -->
+<!-- Here is the images for those items: need about six or so. -->
+<frameset rows="16%,16%,16%,%4,16%,16%,16%" >
+	<frame src="itemA.html" scrolling="no" noresize frameborder="0">
+	<frame src="itemB.html" scrolling="no" noresize frameborder="0">
+	<frame src="itemC.html" scrolling="no" noresize frameborder="0">
+		<frame src="" scrolling="no" noresize frameborder="0"> <!-- This should be blank as the center piece -->
+	<frame src="itemD.html" scrolling="no" noresize frameborder="0">
+	<frame src="itemE.html" scrolling="no" noresize frameborder="0">
+	<frame src="itemF.html" scrolling="no" noresize frameborder="0">
+<!-- Bottom stuffs use frames for lower portion. -->
+</frameset>
+    	 */
     }
     
     // Free the resources associated with the result set
