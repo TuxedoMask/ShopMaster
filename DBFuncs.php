9327f84ccee1b3257cc7fd42ff711c0aa2735f51
Brian <bvest1@umbc.edu>
2012-11-29 19:33:27 -0500

    Fixed a few db functions, fixed product links in pagedview

--- DBFuncs.php
+++ DBFuncs.php
@@ -60,18 +60,18 @@ class DBFuncs
 	//Items a 2d array containing, productId - UnitPrice - Quantity
 	function addOrder($custid, $items, $billInfo, $shipInfo)
 	{	
-		$sql = "INSERT INTO 'ply1'.'Orders' ('CustomerID', 'OrderDate', 'ShipName', 'ShipEmail', 'ShipPhone',
-			'ShipAddress', 'ShipCity', 'ShipCountry', 'ShipPostalCode') VALUES ('".$custid."', '".$shipInfo[0]."',
+		$sql = "INSERT INTO `ply1`.`Orders` (`CustomerID`, `OrderDate`, `ShipName`, `ShipEmail`, `ShipPhone`,
+			`ShipAddress`, `ShipCity`, `ShipCountry`, `ShipPostalCode`) VALUES ('".$custid."', '".$shipInfo[0]."',
 			'".$shipInfo[1]."', '".$shipInfo[2]."', '".$shipInfo[3]."', '".$shipInfo[4]."', '".$shipInfo[5]."',
 			'".$shipInfo[6]."', '".$shipInfo[7]."')";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 
-		$sql = "UPDATE 'ply1'.'Customers' SET 'LastName' = '$billInfo[0]', 'FirstName' = '$billInfo[1]', 'PhoneNo' = '$billInfo[2]',
-			'Address' = '$billInfo[3]', 'City' = '$billInfo[4]', 'Country' = '$billInfo[5]', 'PostalCode' = '$billInfo[5]'
-			WHERE 'CustomerID' = '$custid'";
+		$sql = "UPDATE `ply1`.`Customers` SET `LastName` = '$billInfo[0]', `FirstName` = '$billInfo[1]', `PhoneNo` = '$billInfo[2]',
+			`Address` = '$billInfo[3]', `City` = '$billInfo[4]', `Country` = '$billInfo[5]', `PostalCode` = '$billInfo[5]'
+			WHERE `CustomerID` = '$custid'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
-		$sql = "SELECT 'OrderID' FROM 'ply1'.'Orders' WHERE 'CustomerID' = '$custid'";
+		$sql = "SELECT `OrderID` FROM `ply1`.`Orders` WHERE `CustomerID` = '$custid'";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 
 		while($orderID = mysql_fetch_array($rs)){}
@@ -79,7 +79,7 @@ class DBFuncs
 		//$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]); 
 		for($i = 0; $i <= count($items); $i++)
 		{
-			$sql = "INSERT INTO 'ply1'.'Order Items' ('OrderID', 'ProductID', 'UnitPrice', 'Quantity')
+			$sql = "INSERT INTO `ply1`.`Order Items` (`OrderID`, `ProductID`, `UnitPrice`, `Quantity`)
 				VALUES ('".$orderID."','".$items[$i][0]."','".$items[$i][1]."','".$items[$i][2]."')";
 			$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);	
 		}
@@ -89,7 +89,7 @@ class DBFuncs
 	function getAllProducts()
 	{
 		
-		$sql = "SELECT 'ProductName', 'ProductDesc', 'UnitsInStock', 'Image', 'UnitPrice' FROM 'ply1'.'Products'";
+		$sql = "SELECT `ProductID`,`ProductName`, `ProductDesc`, `UnitsInStock`, `Image`, `UnitPrice` FROM `ply1`.`Products`";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		return $rs;
 	}
@@ -111,13 +111,13 @@ class DBFuncs
 	}
 	function deleteProduct($prodID)
 	{
-		$sql = "DELETE FROM 'ply1'.'Products' WHERE 'ProductID' = '$prodID'";
+		$sql = "DELETE FROM `ply1`.`Products` WHERE `ProductID` = '$prodID'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);	
 	}
 	function updateProducts($prodID, $productArr)
 	{
-		$sql = "UPDATE 'ply1'.'Products' SET 'ProductName' = '$productArr[0]', 'ProductDesc' = '$productArr[1]',
-		'UnitsInStock' = 'productArr[2]', 'Image' = 'productArr[3]' WHERE 'ProductID' = '$prodID'";
+		$sql = "UPDATE `ply1`.`Products` SET `ProductName` = '$productArr[0]', `ProductDesc` = '$productArr[1]',
+		`UnitsInStock` = 'productArr[2]', `Image` = 'productArr[3]' WHERE `ProductID` = '$prodID'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
 	}
