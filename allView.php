ffb4d11ced25c836afa4b72e405f0e7152b47caf
Akiyukio <ply1@umbc.edu>
2012-12-06 00:03:34 -0500

    Fixed allView to conform
    It now looks like paged view

--- allView.php
+++ allView.php
@@ -21,32 +21,42 @@
         // get results from database
         $result = $db->getAllProducts();  
                 
-        // display data in table
-        echo "<p><b>View All</b> | <a href='pagedView.php?page=1'>View Paginated</a></p>";
+
         
-        echo "<table border='1' cellpadding='10'>";
-        echo "<tr> <th>Product</th> <th>Product Description</th> <th>Image of Product</th></tr>";
+//         echo "<table border='1' cellpadding='10'>";
+//         echo "<tr> <th>Product</th> <th>Product Description</th> <th>Image of Product</th></tr>";
 
         // loop through results of database query, displaying them in the table
         $total_results = mysql_num_rows($result);
+        echo '<div id="products">';
         for ($i = 0; $i < $total_results; $i++)
         	{
-            // echo out the contents of each row into a table
-            echo "<tr>";
-            $row = mysql_fetch_array($result);
-            echo '<td><a href="items.php?prodID='.$row['ProductID'].'">'. mysql_result($result, $i, 'ProductName') .'</a></td>';
-//            echo '<td>' . mysql_result($result, $i, 'ProductName') . '</td>';
-            echo '<td>' . mysql_result($result, $i, 'ProductDesc') . '</td>';
-            echo "<td><img src=\"",mysql_result($result, $i, 'Image'), "\" height=\"100\" style=\"max-width: 120px\"></td>";
-            //echo '<td>' . mysql_result($result, $i, 'ImageName') . '</td>';
-            //echo '<td><a href="edit.php?id=' . mysql_result($result, $i, 'id') . '">Edit</a></td>';
-            //echo '<td><a href="delete.php?id=' . mysql_result($result, $i, 'id') . '">Delete</a></td>';
-            echo "</tr>";
-            echo "";
+        	// loop through results of database query, displaying them in the table
+       		echo '<div class="product"><a href="items.php?prodID='.mysql_result($result, $i, 'ProductID').'"><img src='.mysql_result($result, $i, 'Image').'></br>'
+			. mysql_result($result, $i, 'ProductName') .'</a>';
+			echo '</br>$'. mysql_result($result, $i, 'UnitPrice') . '</div>';
         	}
+        echo '</div>';
+        		
+//             // echo out the contents of each row into a table
+//             echo "<tr>";
+//             $row = mysql_fetch_array($result);
+//             echo '<td><a href="items.php?prodID='.$row['ProductID'].'">'. mysql_result($result, $i, 'ProductName') .'</a></td>';
+// //            echo '<td>' . mysql_result($result, $i, 'ProductName') . '</td>';
+//             echo '<td>' . mysql_result($result, $i, 'ProductDesc') . '</td>';
+//             echo "<td><img src=\"",mysql_result($result, $i, 'Image'), "\" height=\"100\" style=\"max-width: 120px\"></td>";
+//             //echo '<td>' . mysql_result($result, $i, 'ImageName') . '</td>';
+//             //echo '<td><a href="edit.php?id=' . mysql_result($result, $i, 'id') . '">Edit</a></td>';
+//             //echo '<td><a href="delete.php?id=' . mysql_result($result, $i, 'id') . '">Delete</a></td>';
+//             echo "</tr>";
+//             echo "";
+//        	}
 
         // close table>
         echo "</table>";
+        
+        // display links to view styles
+        echo "<center><b>View All</b> | <a href='pagedView.php?page=1'>View Paginated</a></center>";
 ?>
 </body>
 </html> 
