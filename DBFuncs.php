77c808df9fcc00f04fbe835dd000f52a126e88b9
Brian <bvest1@umbc.edu>
2012-12-07 00:53:22 -0500

    gfdsdafa

--- DBFuncs.php
+++ DBFuncs.php
@@ -14,6 +14,7 @@ class DBFuncs
 	}
 	function getUserName($userID)
 	{
+		$userID = mysql_real_escape_string($userID);
 		$sql = "SELECT `FirstName` FROM `ply1`.`Customers` WHERE `CustomerID` = '".$userID."'";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		$row = mysql_fetch_row($rs);
@@ -21,14 +22,15 @@ class DBFuncs
 	}
 	function userSignup($email, $pw)
 	{
+		$email = mysql_real_escape_string($email);
+		$pw = mysql_real_escape_string($pw);
 		$sql = "SELECT * FROM `ply1`.`Customers` WHERE `E-mail` = '".$email."'";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		if (mysql_num_rows($rs) > 0)
 		{
 			return 0;
 		}
-		$sql = "INSERT INTO `ply1`.`Customers` (`E-mail`, `Password`)
-			VALUES ('".$email."', '".$pw."')";
+		$sql = "INSERT INTO `ply1`.`Customers` (`E-mail`, `Password`) VALUES ('".$email."', '".$pw."')";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		
 		$sql = "SELECT `CustomerID` FROM `ply1`.`Customers` WHERE `E-mail` = '".$email."'";
@@ -40,6 +42,7 @@ class DBFuncs
 	
 	function userLogin($userName, $pw)
 	{
+		$userName = mysql_real_escape_string($userName);
 		$sql = "SELECT `CustomerID`, `Password` FROM `ply1`.`Customers` WHERE `E-mail` = '".$userName."'";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		$row = mysql_fetch_row($rs);
@@ -57,27 +60,34 @@ class DBFuncs
 	//Items a 2d array containing, productId - UnitPrice - Quantity
 	function addOrder($custid, $items, $billInfo, $shipInfo)
 	{	
-		//echo($items[0] . " " . $items[1]." ".$items[2]);
-		$sql = "INSERT INTO `ply1`.`Orders` (`CustomerID`, `OrderDate`, `ShipName`, `ShipEmail`, `ShipPhone`,
-			`ShipAddress`, `ShipCity`, `ShipCountry`, `ShipPostalCode`, `ShipState`) VALUES ('".$custid."', '$shipInfo[OrderDate]',
-			'".$shipInfo[ShipName]."', '".$shipInfo[ShipEmail]."', '".$shipInfo[ShipPhone]."', '".$shipInfo[ShipAddress]."', '".$shipInfo[ShipCity]."',
-			'".$shipInfo[ShipCountry]."', '".$shipInfo[ShipPostalCode]."', '".$shipInfo[ShipState]."')";
+		$custid = mysql_real_escape_string($custid);
+		echo mysql_real_escape_string($shipInfo[ShipPhone]);
+		$sql = "INSERT INTO `ply1`.`Orders` (`CustomerID`, `OrderDate`, `ShipName`, 
+			`ShipEmail`, `ShipPhone`, `ShipAddress`, `ShipCity`, `ShipCountry`, `ShipPostalCode`, `ShipState`) VALUES 
+			(".$custid.", '$shipInfo[OrderDate]', '".mysql_real_escape_string($shipInfo[ShipName])."', '".mysql_real_escape_string($shipInfo[ShipEmail])."', 
+			'".mysql_real_escape_string($shipInfo[ShipPhone])."', '".mysql_real_escape_string($shipInfo[ShipAddress])."', '".mysql_real_escape_string($shipInfo[ShipCity])."',
+			'".mysql_real_escape_string($shipInfo[ShipCountry])."', ".mysql_real_escape_string($shipInfo[ShipPostalCode]).", '".mysql_real_escape_string($shipInfo[ShipState])."')";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
-		$orderID = mysql_insert_id();
+		$orderID = mysql_insert_id();	
 
-		$sql = "UPDATE `ply1`.`Customers` SET `LastName` = '$billInfo[LastName]', `FirstName` = '$billInfo[FirstName]', `PhoneNo` = '$billInfo[PhoneNo]',
-			`Address` = '$billInfo[Address]', `City` = '$billInfo[City]', `Country` = '$billInfo[Country]', `PostalCode` = '$billInfo[PostalCode]'
-			WHERE `CustomerID` = '$custid'";
+		$sql = "UPDATE `ply1`.`Customers` SET `LastName` = '".mysql_real_escape_string($billInfo[LastName])."', `FirstName` = '".mysql_real_escape_string($billInfo[FirstName])."',
+			`PhoneNo` = '".mysql_real_escape_string($billInfo[PhoneNo])."', `Address` = '".mysql_real_escape_string($billInfo[Address])."',
+			`City` = '".mysql_real_escape_string($billInfo[City])."', `Country` = '".mysql_real_escape_string($billInfo[Country])."', 
+			`PostalCode` = '".mysql_real_escape_string($billInfo[PostalCode])."' WHERE `CustomerID` = '$custid'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
 		
 		for($i = 0; $i < count($items); $i++)
 		{
-			$sql = "INSERT INTO `ply1`.`Order Details` (`OrderID`, `ProductID`, `UnitPrice`, `Quantity`)
-				VALUES ('".$orderID."','".$items[$i++]."','".$items[$i++]."','".$items[$i]."')";
+			$prodID = mysql_real_escape_string($items[$i]);
+			$i++;
+			$price = mysql_real_escape_string($items[$i]);
+			$i++;
+			$quant = mysql_real_escape_string($items[$i]);
+			$sql = "INSERT INTO `ply1`.`Order Details` (`OrderID`, `ProductID`, `UnitPrice`, `Quantity`) VALUES ('".$orderID."','".$prodID."','".$price."','".$quant."')";
 			$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
-			$prodID = $items[$i-2];
+			
 			$quantity = $items[$i];
 			$sql = "SELECT `UnitsInStock` FROM `ply1`.`Products` WHERE `ProductID` = '".$prodID."'";
 			$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
@@ -99,28 +109,37 @@ class DBFuncs
 	//Returns an array
 	function getOneProduct($prodID)
 	{
-		$sql = "SELECT `ProductName`, `ProductDesc`, `UnitsInStock`, `Image`, `UnitPrice` FROM `ply1`.`Products` 
-			WHERE `ProductID` = '".$prodID."'";
+		mysql_real_escape_string($prodID);
+		$sql = "SELECT `ProductName`, `ProductDesc`, `UnitsInStock`, `Image`, `UnitPrice` FROM `ply1`.`Products` WHERE `ProductID` = ".$prodID."";
 		$rs = $this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		return mysql_fetch_array($rs);
 	}
 	function addProduct($productArr)
 	{
-		$sql = "INSERT INTO `ply1`.`Products` (`ProductName`, `ProductDesc`, `UnitsInStock`, `Image`)
-        		VALUES ('".$productArr[0]."', ".$productArr[1].", '".$productArr[2]."', '".$productArr[3]."')";
+		$name = mysql_real_escape_string($productArr[0]);
+		$desc = mysql_real_escape_string($productArr[1]);
+		$units = mysql_real_escape_string($productArr[2]);
+		$image = mysql_real_escape_string($productArr[3]);
+		$sql = "INSERT INTO `ply1`.`Products` (`ProductName`, `ProductDesc`, `UnitsInStock`, `Image`) VALUES ('".$name."', ".$desc.", '".$units."', '".$image."')";
 		
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
 	}
 	function deleteProduct($prodID)
 	{
+		mysql_real_escape_string($prodID);
 		$sql = "DELETE FROM `ply1`.`Products` WHERE `ProductID` = '$prodID'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);	
 	}
 	function updateProducts($prodID, $productArr)
 	{
-		$sql = "UPDATE `ply1`.`Products` SET `ProductName` = '$productArr[0]', `ProductDesc` = '$productArr[1]',
-		`UnitsInStock` = 'productArr[2]', `Image` = 'productArr[3]' WHERE `ProductID` = '$prodID'";
+		$prodID = mysql_real_escape_string($prodID);
+		$name = mysql_real_escape_string($productArr[0]);
+		$desc = mysql_real_escape_string($productArr[1]);
+		$units = mysql_real_escape_string($productArr[2]);
+		$image = mysql_real_escape_string($productArr[3]);
+		$sql = "UPDATE `ply1`.`Products` SET `ProductName` = '$name', `ProductDesc` = '$desc',
+		`UnitsInStock` = '$units', `Image` = '$image' WHERE `ProductID` = '$prodID'";
 		$this->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
 	}
