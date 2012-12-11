88e2dd0e446edf1463f4a7d8c760ed9cea759314
Akiyukio <ply1@umbc.edu>
2012-12-11 13:33:41 -0500

    Updated EditItem.php's message

--- EditItem.php
+++ EditItem.php
@@ -6,14 +6,23 @@ include_once ("layout.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	print 'The item with the following parameters has been updated:<br><br>';
-
-	print 'name = ' . $_POST['name'] . '<br>';
-	print 'desc = ' . $_POST['desc'] . '<br>';
-	print 'price = ' . $_POST['price'] . '<br>';
-	print 'units = ' . $_POST['units'] . '<br>';
-	print 'image = ' . $_POST['imageURL'] . '<br>';
-	print 'featured = ' . $_POST['featured'] . '<br>';
+	print 'The item with the following parameters has been added to the inventory:<br><br>';
+	print '<br>';
+	print 'Product Name:<br>' . $_POST['name'] . '<br>';
+	print '<br>';
+	print 'Description:<br>' . $_POST['desc'] . '<br>';
+	print '<br>';
+	print 'Price:<br>$' . $_POST['price'] . '<br>';
+	print '<br>';
+	print 'Starting Stock:<br>' . $_POST['units'] . '<br>';
+	print '<br>';
+	print 'Image Link:<br>' . $_POST['imageURL'] . '<br>';
+	print '<br>';
+	print 'Featured Item:<br>';
+	if ($_POST['featured'] == 1)
+		print 'Yes' . '<br>';
+	else
+		print 'No' . '<br>';
 
 	$item = array($_POST['name'], $_POST['desc'], $_POST['price'], $_POST['units'], $_POST['imageURL'], $_POST['featured']);
 
